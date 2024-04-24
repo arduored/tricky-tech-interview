44d3633ab841a62827fe9891e73f0d5522edd5e7
arduored <edouard.souan@zohomail.eu>
2024-04-24 22:13:04 +0200

    Chore: complete rules

--- front/app/rules/page.tsx
+++ front/app/rules/page.tsx
@@ -3,7 +3,7 @@ export default function Rules() {
     <div className="flex flex-col my-10">
       <h3 className="text-3xl mb-20"># - Rules</h3>
 
-      <ol className="list-decimal list-inside leading-loose">
+      <ol className="list-decimal list-inside leading-loose flex flex-col gap-4">
         <li>The goal is to make the maximum of point.</li>
 
         <li>
@@ -11,9 +11,14 @@ export default function Rules() {
           different amount of points.
         </li>
         <li>
-          White tasks take longer to destroy but give 0 point. If an white task
+          White tasks take longer to destroy but give 0 point. If a white task
           riches the ground, it gets stuck and prevent you from moving passed
           it. To destroy it, you must shoot an white circle.
+        </li>
+        <li>
+          For each task that passes behind your ship, you'll lose a vacation
+          day. You have 3 vacation day to start. One more mistake after that and
+          its the burnout!
         </li>
       </ol>
     </div>
